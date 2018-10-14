031a46e3f469826d1fb7212b1d4bf3f26362339c
Dafei Ning <ningdafei@gmail.com>
2018-10-14 02:59:44 -0700

    send notificcation

--- EventReporter2/app/src/main/java/dafeining/eventreporter2/MyFirebaseMessagingService.java
+++ EventReporter2/app/src/main/java/dafeining/eventreporter2/MyFirebaseMessagingService.java
@@ -4,7 +4,13 @@ this service manages the response of notifications from FCM
 
 package dafeining.eventreporter2;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -34,5 +40,38 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
     }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     */
+    private void sendNotification(String fcmmessage) {
+        Intent intent = new Intent(this, ControlPanel.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        //Define pending intent to trigger activity
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 , intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        //Create Notification according to builder pattern
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, "EventReporter");
+        notificationBuilder
+                .setSmallIcon(R.drawable.witcher3_icon)
+                .setContentTitle("FCM Message")
+                .setContentText(fcmmessage)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        // Get Notification Manager
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Send notification
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
 
 }
